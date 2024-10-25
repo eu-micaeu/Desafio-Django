64377ee6e13d326326375feea9652c7da1cdce74
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2024-10-25 02:01:47 -0300

    Refactor project structure and remove unused files

--- myproject/todo/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import TodoItem
-
-def index(request):
-    todo_items = TodoItem.objects.all()
-    return render(request, 'todo/index.html', {'todo_items': todo_items})
