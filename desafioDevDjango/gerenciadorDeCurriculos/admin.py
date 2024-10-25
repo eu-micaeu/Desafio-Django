23fad8229aee8b20c03637da95899062704f1326
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2024-10-25 02:40:24 -0300

    Refactor admin.py to register new models

--- desafioDevDjango/gerenciadorDeCurriculos/admin.py
+++ desafioDevDjango/gerenciadorDeCurriculos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import DadosPessoais, Contato, ExperienciaProfissional, FormacaoAcademica
 
-# Register your models here.
+admin.site.register(DadosPessoais)
+admin.site.register(Contato)
+admin.site.register(ExperienciaProfissional)
+admin.site.register(FormacaoAcademica)
