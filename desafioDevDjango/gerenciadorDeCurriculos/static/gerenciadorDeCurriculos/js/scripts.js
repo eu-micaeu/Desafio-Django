4e7804d6082b9d8dad6f048ce94e2e12c2c6f22e
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2024-10-27 23:03:50 -0300

    Refactor form inputs to add placeholder text Refactor form submission and clear fields after successful submission Refactor models to remove unused fields and dependencies Refactor project structure and add static files and templates Refactor admin.py to register new models

--- desafioDevDjango/gerenciadorDeCurriculos/static/gerenciadorDeCurriculos/js/scripts.js
+++ desafioDevDjango/gerenciadorDeCurriculos/static/gerenciadorDeCurriculos/js/scripts.js
@@ -1,175 +1,72 @@
 const csrftoken = document.querySelector('[name=csrf-token]').content;
 
-document.getElementById("btEnter").addEventListener("click", function(){
-
-    var nome = document.getElementById('nome');
-    var sobrenome = document.getElementById('sobrenome');
-    var dataDeNascimento = document.getElementById('dataDeNascimento');
-
-    fetch('http://127.0.0.1:8000/api/dados-pessoais/', {
-
-        method: 'POST',
-
-        headers: {
-
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrftoken
-        },
-
-        body: JSON.stringify({
-
-            nome: nome.value,
-
-            sobrenome: sobrenome.value,
-
-            data_de_nascimento: dataDeNascimento.value
-
-        })
-
-    })
-
-    .then(response => response.json())
-
-    .then(data => {
-
-        console.log('Success:', data);
-
-    });
-
-    var email = document.getElementById('email');
-    var telefone = document.getElementById('telefone');
-    var endereco = document.getElementById('endereco');
-
-    fetch('http://127.0.0.1:8000/api/contato/', {
-
-        method: 'POST',
-
-        headers: {
-
-            'Content-Type': 'application/json',
-
-            'X-CSRFToken': csrftoken
-
-        },
-
-        body: JSON.stringify({
-
-            email: email.value,
-
-            telefone: telefone.value,
-
-            endereco: endereco.value
-
-        })
-
-    })
-
-    .then(response => response.json())
-
-    .then(data => {
-
-        console.log('Success:', data);
-        
-    });
-
-    var cargo = document.getElementById('cargo');
-    var empresa = document.getElementById('empresa');
-    var dataDeInicio = document.getElementById('dataDeInicio');
-    var dataDeTermino = document.getElementById('dataDeTermino');
-    var descricao = document.getElementById('descricao');
-
-    fetch('http://127.0.0.1:8000/api/experiencia-profissional/', {
-
-        method: 'POST',
-
-        headers: {
-
-            'Content-Type': 'application/json',
-
-            'X-CSRFToken': csrftoken
-
-        },
-
-        body: JSON.stringify({
-
-            cargo: cargo.value,
-
-            empresa: empresa.value,
-
-            data_inicio: dataDeInicio.value,
-
-            data_fim: dataDeTermino.value,
-
-            descricao: descricao.value
-
-        })
-
-    })
-
-    .then(response => response.json())
-
-    .then(data => {
-
-        console.log('Success:', data);
-        
-    });
-
-    var instituicao = document.getElementById('instituicao');
-    var curso = document.getElementById('curso');
-    var dataDeInicioCurso = document.getElementById('dataDeInicioCurso');
-    var dataDeTerminoCurso = document.getElementById('dataDeTerminoCurso');
-
-    fetch('http://127.0.0.1:8000/api/formacao-academica/', {
-
-        method: 'POST',
-
-        headers: {
-
-            'Content-Type': 'application/json',
-
-            'X-CSRFToken': csrftoken
-
-        },
-
-        body: JSON.stringify({
-
-            instituicao: instituicao.value,
-
-            curso: curso.value,
-
-            data_inicio: dataDeInicioCurso.value,
-
-            data_fim: dataDeTerminoCurso.value
-
-        })
-
-    })
-
-    .then(response => response.json())
-
-    .then(data => {
-
-        console.log('Success:', data);
-        
-    });
-
-    alert('Currículo cadastrado com sucesso!');
-
-    nome.value = '';
-    sobrenome.value = '';
-    dataDeNascimento.value = '';
-    email.value = '';
-    telefone.value = '';
-    endereco.value = '';
-    cargo.value = '';
-    empresa.value = '';
-    dataDeInicio.value = '';
-    dataDeTermino.value = '';
-    descricao.value = '';
-    instituicao.value = '';
-    curso.value = '';
-    dataDeInicioCurso.value = '';
-    dataDeTerminoCurso.value = '';
-
+const clearFields = () => {
+    const fields = [
+        'nome', 'sobrenome', 'dataDeNascimento', 'email', 'telefone', 'endereco',
+        'cargo', 'empresa', 'dataDeInicio', 'dataDeTermino', 'descricao',
+        'instituicao', 'curso', 'dataDeInicioCurso', 'dataDeTerminoCurso'
+    ];
+    fields.forEach(fieldId => document.getElementById(fieldId).value = '');
+};
+
+const validateFields = (data) => {
+    return Object.values(data).every(value => value.trim() !== '');
+};
+
+const postData = (url, data) => {
+    if (validateFields(data)) {
+        return fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrftoken
+            },
+            body: JSON.stringify(data)
+        }).then(response => response.json());
+    } else {
+        console.error('Todos os campos devem estar preenchidos');
+        return Promise.reject('Campos em branco');
+    }
+};
+
+document.getElementById("btEnter").addEventListener("click", () => {
+    const dataPessoais = {
+        nome: document.getElementById('nome').value,
+        sobrenome: document.getElementById('sobrenome').value,
+        data_de_nascimento: document.getElementById('dataDeNascimento').value
+    };
+
+    const contato = {
+        email: document.getElementById('email').value,
+        telefone: document.getElementById('telefone').value,
+        endereco: document.getElementById('endereco').value
+    };
+
+    const experienciaProfissional = {
+        cargo: document.getElementById('cargo').value,
+        empresa: document.getElementById('empresa').value,
+        data_inicio: document.getElementById('dataDeInicio').value,
+        data_fim: document.getElementById('dataDeTermino').value,
+        descricao: document.getElementById('descricao').value
+    };
+
+    const formacaoAcademica = {
+        instituicao: document.getElementById('instituicao').value,
+        curso: document.getElementById('curso').value,
+        data_inicio: document.getElementById('dataDeInicioCurso').value,
+        data_fim: document.getElementById('dataDeTerminoCurso').value
+    };
+
+    Promise.all([
+        postData('http://127.0.0.1:8000/api/dados-pessoais/', dataPessoais),
+        postData('http://127.0.0.1:8000/api/contato/', contato),
+        postData('http://127.0.0.1:8000/api/experiencia-profissional/', experienciaProfissional),
+        postData('http://127.0.0.1:8000/api/formacao-academica/', formacaoAcademica)
+    ]).then(responses => {
+        console.log('Success:', responses);
+        alert('Currículo cadastrado com sucesso!');
+        clearFields();
+    }).catch(error => console.error('Error:', error));
 });
 
+document.getElementById("btClear").addEventListener("click", clearFields);
